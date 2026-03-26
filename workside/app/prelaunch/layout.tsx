4b805a59919b76a6d9d81e89c7b3ed2654e7aad4
Worksidecoach <careerstyling@gmail.com>
2026-03-26 17:27:58 +0900

    fix: KakaoSDK를 Client Component로 분리 (빌드 에러 수정)

--- workside/app/prelaunch/layout.tsx
+++ workside/app/prelaunch/layout.tsx
@@ -1,8 +1,8 @@
 // @TASK P2-S0-T1 - Prelaunch 전용 레이아웃
 // @SPEC docs/planning/prelaunch/layout
 
-import Script from 'next/script';
 import SingleColumnLayout from '@/components/layout/SingleColumnLayout';
+import { KakaoSDK } from '@/components/prelaunch/KakaoSDK';
 
 interface PrelaunchLayoutProps {
   children: React.ReactNode;
@@ -11,17 +11,7 @@ interface PrelaunchLayoutProps {
 export default function PrelaunchLayout({ children }: PrelaunchLayoutProps) {
   return (
     <>
-      <Script
-        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.4/kakao.min.js"
-        integrity="sha384-DKYJZ8NLiK8MN4/C5P2ezmFnkrysYczQosi1y6YC9mPkHEHoKEJf/ILjYKMmco4"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-        onLoad={() => {
-          if (typeof window !== 'undefined' && (window as any).Kakao && !(window as any).Kakao.isInitialized()) {
-            (window as any).Kakao.init('7be88ea0292320f0de3f6accce9ffd7c');
-          }
-        }}
-      />
+      <KakaoSDK />
       <SingleColumnLayout>{children}</SingleColumnLayout>
     </>
   );
